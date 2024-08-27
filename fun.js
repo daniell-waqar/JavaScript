026c7e7e4b9bf33b96a0c54c60b800b5132cb0ec
daniell-waqar <p200750@pwr.nu.edu.pk>
2024-08-27 20:33:32 +0500

    initial commit

---
+++ fun.js
@@ -0,0 +1,18 @@
+
+//Write a function to find the count of a letter in a string
+
+
+
+function letterCount(str, c) {
+  var count=0;
+  str=str.toLowerCase(); 
+  for(var i=0;i<str.length;i++){
+     if(str.charAt(i)==c){ 
+       count++; 
+     }
+  }
+  return count;
+}
+
+console.log(letterCount("Connect",'c'))
+console.log(letterCount("first person",'s')) 
